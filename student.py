ba0a10a70a39a7861680b53007dd2c1b7389e3f6
doaa-altarawy <doaa.altarawy@gmail.com>
2017-08-02 08:25:15 -0400

    Add empty student class and tests

---
+++ student.py
@@ -0,0 +1,8 @@
+"""
+A module for handling school students related information
+"""
+
+
+class Student(object):
+    """Basic student class"""
+    pass
---
+++ tests/test_student.py
@@ -0,0 +1,36 @@
+import pytest
+# from student import Student, GradStudent, UndergradStudent
+
+
+# s1 = GradStudent(name='Joe Smith', ID='453535', GPA=2.7)
+# s2 = UndergradStudent(name='Sally Smith', ID='453535', GPA=2.7)
+
+
+def test_name():
+    # assert s1.name == 'Joe Smith'
+    # assert s2.name == 'Sally Smith'
+    pass
+
+
+def test_GPA():
+    # assert s1.get_GPA() == 2.7
+    # assert s2.get_GPA() == 2.7
+    pass
+
+def test_pass():
+    # assert not s1.is_pass()
+    # assert s2.is_pass()
+    pass
+
+
+def test_advisor():
+    # advisor = 'Shaffer'
+    # s1.set_advisor(advisor)
+    # assert s1.get_advisor() == advisor
+    pass
+
+
+def test_ID():
+    # with pytest.raises(AttributeError):
+    #     id = s1.__ID()
+    pass
